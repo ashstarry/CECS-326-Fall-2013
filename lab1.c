d5085164f2833d4f10044185cf29a82e0d34c35f
Steven Le <stevenhieule@yahoo.com>
2013-09-14 22:44:50 -0700

    comments comments formatting

--- lab1.c
+++ lab1.c
@@ -11,44 +11,44 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-int* newRandomArray(int);
+int* newRandomArray(int);	// Function prototype
 
 int main(int argc, char *argv[])
 {
 	int i,j, n, sum, max, min;
 	sum = 0;    // Initialize sum to be zero
 	int *randA;
-	double average = 0.0;
+	double average = 0.0;	// Initialize average to double zero
 	if (argc !=2) // If argument character is not two fields, print statement
 	{
 		printf("\n Usage: Need filename and one argument\n", argv[0]);
 		exit(1);
 	}
-	n = atoi(argv[1]); // Index 2 (number N)
+	n = atoi(argv[1]); // n receives index 2
 	srand(time(NULL)); // Seed, initialize random number generator
-	randA = newRandomArray(n);
-	max = randA[0]; // First random number will be max if greater than -100 range
-	min = randA[0];  // First random number will be min if less than 100 range
-	for (i=0; i<n;i++) // Start at i = 0, iterate to # n random numbers
+	randA = newRandomArray(n);	// function newRandomArray with n assign to randA
+	max = randA[0]; // First random number will be max at array index 0
+	min = randA[0];  // First random number will be min at array index 0
+	for (i = 0; i < n; i++) // Start at i = 0, iterate to # n random numbers
 	{
-        if(randA[i]>max)
+        if(randA[i] > max)
         {
-            max=randA[i]; // If number generate is > max, replace max
+            max=randA[i]; // If number generate is > max, replace max with value
         }
-        else if(randA[i]<min)
+        else if(randA[i] < min)
         {
-            min=randA[i]; // If number generate is < max, replace min
+            min=randA[i]; // If number generate is < max, replace min with value
         }
         else
         {
-            max = max;
+            max = max;	// If number generated is equal to max or min, keep value
             min = min;
         }
-        sum = randA[i] + sum;  // Place hold a sum of all random integers
-	}
+        sum = randA[i] + sum;  // Temp variable to hold sum of all random integers in array
+	}	// End for-loop
 	printf ("\n");
-	average = (sum/(double)n); // Calculate average by sum of random integers
-	for (j=0; j < n; j++)
+	average = (sum/(double)n); // Calculate average = sum of random integers / n
+	for (j = 0; j < n; j++)
 	{
 	    printf("%d ", randA[j]);     // Display the array of random integers
     }
@@ -56,7 +56,7 @@ int main(int argc, char *argv[])
     printf("\nMaximum of random integers is: %d", max, "\n");
     printf("\nMinimum of random integers is: %d", min, "\n");
     printf ("\n");
-    free(randA);     // Free memory
+    free(randA);     // Deallocate memory block
 	exit(0);
 }
 
@@ -64,7 +64,7 @@ int* newRandomArray(int n)
 {
      int* h = (int *) malloc(n*sizeof(int)); // malloc allocates memory for array
      int i;
-     for (i=0; i<n; ++i)
-         h[i] = rand()%201-100;              // Sets range for -100 to 100
-         return h;
+     for (i = 0; i < n; ++i)
+         h[i] = rand() % 201-100;            // Sets range for -100 to 100
+         return h;							 // Returns a pointer
 }
