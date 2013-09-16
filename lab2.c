6580ea84d1a4653abd8ca5567c5db1e443566364
Steven Le <stevenhieule@yahoo.com>
2013-09-15 21:21:02 -0700

    change header

--- lab2.c
+++ lab2.c
@@ -1,9 +1,12 @@
 /**************************************************************************/ 
-/* PROGRAM: lab2.c */ 
-/* DESCRIPTION: This program generates a chain of processes */ 
-/* using fork(). The number of processes n is a command line argument. */ 
-/* Each process sleeps for a random time less than 10 seconds then prints out */ 
-/* process ID, parent ID, and child ID */ 
+/* PROGRAM NAME: lab2.c
+/* CLASS: CECS-326
+/* INSTRUCTOR: Mr. Haney Williams
+/* STUDENT: Steven Le
+/* DESCRIPTION: This program generates a chain of processes
+/* using fork(). The number of processes n is a command line argument.
+/* Each process sleeps for a random time less than 10 seconds then prints out
+/* process ID, parent ID, and child ID
 /**************************************************************************/ 
 # include <stdio.h> 
 # include <stdlib.h> 
