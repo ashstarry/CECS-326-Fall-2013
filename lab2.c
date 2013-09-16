3ce1bb3e32109b308d2462aa046176e0d739bab4
Steven Le <stevenhieule@yahoo.com>
2013-09-15 20:51:52 -0700

    formatting and tabs

--- lab2.c
+++ lab2.c
@@ -10,31 +10,33 @@
 # include <sys/types.h> 
 # include <unistd.h> 
 int main(int argc, char *argv[]) 
-{ 
- int i, m, n, sleeptime, seed; 
- pid_t childpid; 
- if (argc !=2) { 
- printf("\n Usage: %s processes\n", argv[0]); 
- exit(1); 
- } 
- n = atoi(argv[1]); m = 10; 
- childpid = 0; 
- for (i=0; i<n;i++) 
- if (childpid = fork()) break; 
- if (childpid == -1) { 
- perror ("\n The fork failed\n"); 
- exit(1); 
- } 
- printf("\n %d: process ID:%6ld parent ID:%6ld child 
-ID:%6ld",i, (long)getpid(), (long)getppid(), (long)childpid); 
- seed = (int)(getpid() + childpid); 8
- srand(seed); 
- /* since each process has a different childpid, using the childpid 
- as the seed number will restart the random function. 
- Therefore, each process will have a different sleeptime 
- */ 
- sleeptime = rand()%m; 
- printf (" sleep = %d\n", sleeptime); 
- sleep(sleeptime); 
- exit(0); 
+{
+    int i, m, n, sleeptime, seed; 
+    pid_t childpid; 
+    if (argc !=2) 
+    { 
+      printf("\n Usage: %s processes\n", argv[0]); 
+      exit(1); 
+    } 
+    n = atoi(argv[1]); m = 10; 
+    childpid = 0; 
+    for (i=0; i<n;i++) 
+        if (childpid = fork()) break; 
+    if (childpid == -1)
+    { 
+      perror ("\n The fork failed\n"); 
+      exit(1); 
+    } 
+    printf("\n %d: process ID:%6ld parent ID:%6ld child 
+    ID:%6ld",i, (long)getpid(), (long)getppid(), (long)childpid); 
+    seed = (int)(getpid() + childpid); 8
+    srand(seed); 
+     /* since each process has a different childpid, using the childpid 
+     as the seed number will restart the random function. 
+     Therefore, each process will have a different sleeptime 
+     */ 
+    sleeptime = rand()%m; 
+    printf (" sleep = %d\n", sleeptime); 
+    sleep(sleeptime); 
+    exit(0); 
 }
