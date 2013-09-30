f62cbae00818fe92c4e226db2b139531f3e8d65b
Steven Le <stevenhieule@yahoo.com>
2013-09-30 14:47:06 -0700

    started lab3
    paste example

---
+++ lab3.c
@@ -0,0 +1,28 @@
+/****************************************************************/
+/*	PROGRAM NAME: lab3.c
+/*	CLASS: CECS-326
+/*	INSTRUCTOR: Mr. Haney Williams
+/*	STUDENT: Steven Le
+/*	DESCRIPTION: A process creates a child process and may want
+/*  to change the program code the child is executing. There
+/*  are 6 system calls under the generalized heading exec. When 
+/*  the process issues any exec call, if the call is successful,
+/*  the existing process is overlaid with a new set of program
+/*  code.
+/****************************************************************/
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <unistd.h>
+
+void main(int argc, char *argv[])
+{
+     if (argc > 1)
+     {
+        execlp("/bin/cat", "cat", argv[1], (char *) NULL);
+        perror ("exec failure");
+        exit(1);
+     }
+     printf("\n Usage: %s text_file\n", *argv);
+     exit(1) ;
+}
