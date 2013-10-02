4904c605e2d766d61a280ce9f0519336c810eed8
Steven Le <stevenhieule@yahoo.com>
2013-10-01 18:56:17 -0700

    part b

---
+++ lab3b.c
@@ -0,0 +1,32 @@
+/****************************************************************/
+/*	PROGRAM NAME: lab3.c
+/*	CLASS: CECS-326
+/*	INSTRUCTOR: Mr. Haney Williams
+/*	STUDENT: Steven Le
+/*	DESCRIPTION: A process creates a child process and may want
+/*  to change the program code the child is executing. There
+/*  are 6 system calls under the generalized heading exec. When 
+/*  the process issues any exec call, if the call is successful,
+/*  the existing process is overlaid with a new set of program
+/*  code.
+/****************************************************************/
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <unistd.h>
+
+void main(int argc, char *argv[])
+{
+     if (argc > 1)
+     {
+         static char *new_argv[] = {"cat", "lab1.c", (char *)0};
+         execvp("/bin/cat", new_argv);
+         perror("exec failure");
+         exit(1);
+  //      execvp(argv[1], &argv[1]);
+   //     perror ("exec failure");
+   //     exit(1);
+     }
+     printf("\n Usage: %s text_file\n", *argv);
+     exit(1) ;
+}
