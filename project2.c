6cbba6a2c9ae13294722ae1ec2babf542a083f69
Steven Le <stevenhieule@yahoo.com>
2013-10-09 14:36:59 -0700

    cody's sleeptime

--- project2.c
+++ project2.c
@@ -1,10 +1,10 @@
 /**************************************************************************/ 
-/* PROGRAM NAME: project2.c
-/* CLASS:        CECS-326
-/* INSTRUCTOR:   Mr. Haney Williams
-/* STUDENTS:     Cody Gildea
-/*               Steven Le
-/* DESCRIPTION: This program utilizes a parent program and child program.
+/* PROGRAM NAME: project2.c						                          */
+/* CLASS:        CECS-326						                          */
+/* INSTRUCTOR:   Mr. Haney Williams					                      */
+/* STUDENTS:     Cody Gildea						                      */
+/*               Steven Le						                          */
+/* DESCRIPTION: This program utilizes a parent program and child program. */
 /**************************************************************************/ 
 # include<stdio.h>
 # include<stdlib.h>
@@ -13,17 +13,18 @@
 
 int addition(int i0, int i1);
 int diff(int i0, int i1);
-int divide(int i0, int i1);
-int multiply(int i0, int i1);
+double divide(int i0, int i1);
+int  multiply(int i0, int i1);
 
 int main(int argc, char *argv[]) 
 {
     int i, i0, i1, m, n, sleeptime, seed; 
-    double sum, difference, quotient, product;
+    int sum, difference, product;
+    double quotient;
     pid_t childpid; 
     if (argc !=4) // If arguments are not 4 inputs
     { 
-      printf("Invalid inputs, require 4 arguments.\n", argv[0]); 
+      printf("Invalid inputs, requires 4 arguments.\n", argv[0]); 
       exit(1); 
     } 
     n = 4;     // Number of processes = 4
@@ -33,7 +34,7 @@ int main(int argc, char *argv[])
     if (m > 50 || m <= 0) // If argument m is greater than 50, 0, or negative, throw exception
     { 
       printf("Input error.\n");
-      printf("Maximum sleeptime m must be < 50 and > 0.\n"); 
+      printf("Maximum sleeptime m must be <= 50 and > 0.\n"); 
       exit(1); 
     }
     sum = addition(i0, i1);
@@ -53,34 +54,35 @@ int main(int argc, char *argv[])
         { 
           perror ("The fork failed.\n"); 
           exit(1); 
-          } 
-        sleep(m);
+        }
+ 	seed = (int)(getpid() + childpid);
+    	srand(seed); 
+     	/* since each process has a different childpid, using the childpid 
+     	as the seed number will restart the random function. 
+     	Therefore, each process will have a different sleeptime 
+     	*/ 
+    	sleeptime = rand()%m; 
+   	sleep(sleeptime); 
+       
         switch(i)
         {
          case 0:
-              printf("I am child number %d with PID %d, the sum is &d", i, (long)getpid(), sum); 
-              return(1);
+              printf("I am child number %d with PID %d, the sum is %d\n", i, (long)getpid, sum); 
+              break;
          case 1:
-              printf("I am child number %d with PID %d, the difference is &d", i, (long)getpid(), difference);
-              return(1);
+              printf("I am child number %d with PID %d, the difference is %d\n", i, (long)getpid, difference);
+              break;
          case 2:
-              printf("I am child number %d with PID %d, the quotient is &d", i, (long)getpid(), quotient);
-              return(1);
+              printf("I am child number %d with PID %d, the quotient is %lf\n", i, (long)getpid, quotient);
+              break;
          case 3:
-              printf("I am child number %d with PID %d, the product is &d", i, (long)getpid(), product);
-              return(1);     
+              printf("I am child number %d with PID %d, the product is %d\n", i, (long)getpid, product);
+              break;
+	 default:
+	      printf("An error has occured");
+	      exit(1);     
          }    // End switch
     } // End for
-    printf("%d: process ID:%6ld parent ID:%6ld child ID:%6ld",i, (long)getpid(), (long)getppid(), (long)childpid); 
-    seed = (int)(getpid() + childpid);
-    srand(seed); 
-     /* since each process has a different childpid, using the childpid 
-     as the seed number will restart the random function. 
-     Therefore, each process will have a different sleeptime 
-     */ 
-    sleeptime = rand()%m; 
-    sleep(sleeptime);  
-    printf (" sleep = %d\n", sleeptime);
     exit(0);
 }
 int addition(int i0, int i1)
@@ -91,11 +93,12 @@ int diff(int i0, int i1)
 {
     return i0 - i1;   
 }
-int divide(int i0, int i1)
+double divide(int i0, int i1)
 {
-    return i0 / i1;   
+    return (double)i0 /(double) i1;   
 }
 int multiply(int i0, int i1)
 {
     return i0 * i1;   
 }
+
