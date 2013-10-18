ce2453448c535dd1ebd2e0e488ddccf6ff0659e3
Steven Le <stevenhieule@yahoo.com>
2013-10-18 16:01:32 -0700

    copied parent and child for lab5 template

---
+++ lab5child.c
@@ -0,0 +1,32 @@
+/**************************************************************************/ 
+/* PROGRAM NAME: lab5parent.c
+/* CLASS: CECS-326
+/* INSTRUCTOR: Mr. Haney Williams
+/* STUDENT: Steven Le
+/* DESCRIPTION: This program utilizes the concept of a parent generating
+/* a child process and then overlays it by a call to exec.
+/**************************************************************************/ 
+# include <stdio.h> 
+# include <stdlib.h> 
+# include <sys/types.h> 
+# include <unistd.h> 
+# include <stdio.h>
+
+main(int argc, char *argv[])
+{
+   pid_t pid; int ret_value;
+   pid = getpid();
+   ret_value = (int) (pid %256);
+   srand(unsigned) pid);
+   sleep(rand() %5);
+   if (atoi(*(argv + 1)) %2)
+   {
+      printf(“Child %d is terminating with signal 009\n”, pid);
+      kill(pid, 9);
+   }
+   else
+   {
+       printf(“Child %d is terminating with exit(%04X)\n”, pid, ret_value);
+       exit(ret_value);
+   }
+}
---
+++ lab5parent.c
@@ -0,0 +1,36 @@
+/**************************************************************************/ 
+/* PROGRAM NAME: lab5parent.c
+/* CLASS: CECS-326
+/* INSTRUCTOR: Mr. Haney Williams
+/* STUDENT: Steven Le
+/* DESCRIPTION: This program utilizes the concept of a parent generating
+/* a child process and then overlays it by a call to exec.
+/**************************************************************************/ 
+# include <stdio.h> 
+# include <stdlib.h> 
+# include <sys/types.h> 
+# include <unistd.h> 
+# include <stdio.h>
+
+main(void)
+{
+   pid_t pid, w; int k, status; char value[3];
+   for (k=o;k < 3; ++k)
+   {
+      if ((pid = fork()) == 0)
+      {
+         sprintf(value, “%d”,k);
+         execl(“child”, “child”, value, (char *) 0);
+      }
+      else
+      printf (“Forked child %d\n”, pid);
+   }
+
+/* Wait for children */
+    while ((w = wait(&status)) && w != - 1)
+    {
+       if (w != - 1)
+          printf (“Wait on PID: %d returns status of: %04X\n”, w, status);
+    }
+    exit(0);
+}
