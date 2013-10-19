af847097e2a178372fd1b8d0c209075e74d6a143
Steven Le <stevenhieule@yahoo.com>
2013-10-18 17:38:49 -0700

    " errors and parentheses

--- lab5child.c
+++ lab5child.c
@@ -12,21 +12,21 @@
 # include <unistd.h> 
 # include <stdio.h>
 
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
    pid_t pid; int ret_value;
    pid = getpid();
    ret_value = (int) (pid %256);
-   srand(unsigned) pid);
+   srand((unsigned) pid);
    sleep(rand() %5);
    if (atoi(*(argv + 1)) %2)
    {
-      printf(“Child %d is terminating with signal 009\n”, pid);
+      printf("Child %d is terminating with signal 009\n", pid);
       kill(pid, 9);
    }
    else
    {
-       printf(“Child %d is terminating with exit(%04X)\n”, pid, ret_value);
+       printf("Child %d is terminating with exit(%04X)\n", pid, ret_value);
        exit(ret_value);
    }
 }
--- lab5parent.c
+++ lab5parent.c
@@ -15,22 +15,22 @@
 main(void)
 {
    pid_t pid, w; int k, status; char value[3];
-   for (k=o;k < 3; ++k)
+   for (k=0;k < 3; ++k)
    {
       if ((pid = fork()) == 0)
       {
-         sprintf(value, “%d”,k);
-         execl(“child”, “child”, value, (char *) 0);
+         sprintf(value, "d",k);
+         execl("child", "child", value, (char *) 0);
       }
       else
-      printf (“Forked child %d\n”, pid);
+      printf ("Forked child %d\n", pid);
    }
 
 /* Wait for children */
     while ((w = wait(&status)) && w != - 1)
     {
        if (w != - 1)
-          printf (“Wait on PID: %d returns status of: %04X\n”, w, status);
+          printf ("Wait on PID: %d returns status of: %04X\n", w, status);
     }
     exit(0);
 }
