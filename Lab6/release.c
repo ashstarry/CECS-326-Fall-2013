bbc2276fdac4a541af20fd4dad8dbff067a0b5bf
Steven Le <stevenhieule@yahoo.com>
2013-10-28 16:39:25 -0700

    release.c template for lab 6

---
+++ Lab6/release.c
@@ -0,0 +1,30 @@
+/**************************************************************************/ 
+/* PROGRAM NAME: release.c
+/* CLASS: CECS-326
+/* INSTRUCTOR: Mr. Haney Williams
+/* STUDENT: Steven Le
+/* DESCRIPTION: 
+/**************************************************************************/ 
+# include <stdio.h> 
+# include <stdlib.h>
+# include <fcntl.h> 
+# include <errno.h>
+# include <sys/types.h> 
+
+int main(int argc, char *argv[])
+{
+   char *fname; int fd, sleeptime, n_try, count=0; pid_t pid;
+   pid = getpid();
+   srand((unsigned) pid);
+   fname = argv[1];
+   sleeptime = atoi(argv[2]);
+   n_try = atoi(argv[3]);
+   while (unlink(fname)!=0)
+   if (++count < n_try)
+      sleep(sleeptime);
+   else
+   {
+      printf ("\n Cannot release file\n"); exit(-1);
+   }
+   printf ("\n File is released\n");
+}
