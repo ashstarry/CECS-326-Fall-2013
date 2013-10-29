c6cf246be335559b1d704fb998140c4d5967ce1a
Steven Le <stevenhieule@yahoo.com>
2013-10-28 23:16:52 -0700

    rename and conform to cody

--- Project3/proj3c.c
+++ Project3/proj3c.c
@@ -65,7 +65,7 @@ main(int argc, char *argv[])
                  if ((pid_c = fork()) == 0)
                  {
                         sprintf(sleepChar, "%d", sleeptime);
-                        execlp("Project3", "release", lockfname, sleepChar, "1", (char*)0);
+                        execlp("release", "release", lockfname, sleepChar, "1", (char*)0);
                  }
                  while ((w=wait(&status)) && w != - 1)
                 {
