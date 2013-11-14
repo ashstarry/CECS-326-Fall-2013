8f431c413ab0dc5a281b8d631a4ab12e5eff6f77
Steven Le <stevenhieule@yahoo.com>
2013-11-13 20:33:34 -0800

    semaphore template

--- Project4/proj4.c
+++ Project4/proj4.c
@@ -1,5 +1,5 @@
 /**************************************************************************/ 
-/* PROGRAM NAME: semcontrol.c
+/* PROGRAM NAME: proj4.c
 /* CLASS:        CECS-326
 /* INSTRUCTOR: Mr. Haney Williams
 /* STUDENT: Steven Le
@@ -24,11 +24,36 @@ union semun
 int main(void)
 {
    int sem_id, sem_value, i;
+   int counter;
    key_t ipc_key;
    struct semid_ds sem_buf;
-   static ushort sem_array[NS] = {3, 1, 4};
+// static ushort sem_array[NS] = {3, 1, 4};
+   static ushort sem_array[NS];
    union semun arg;
    ipc_key = ftok(".", 'S');
+
+    if(argv[1] == 'r')
+       ropt = atoi(argv[1])
+    else if(argv[1] == 'n')
+       ropt = atoi(argv[1])
+    else
+    {
+      printf("Input error.\n");
+      printf("Option is not valid.\n"); 
+      exit(1);         
+    }
+    NS = atoi(argv[2]);    // Argument index 2 is number of semaphores
+    for (counter = 0; counter < NS; ++counter)
+    A = atoi(argv[2]);    // Argument index 2 is first integer
+    B = atoi(argv[3]);    // Argument index 3 is second integer
+    if (T > 50 || T <= 0) // If argument T is greater than 50, 0, or negative, throw exception
+    { 
+      printf("Input error.\n");
+      printf("Maximum sleeptime m must be <= 50 and > 0.\n"); 
+      exit(1); 
+    }
+
+
 /* Create semaphore */
    if ((sem_id = semget(ipc_key, NS, IPC_CREAT | 0666)) == -1)
    {
